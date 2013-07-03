e26cca76a7779289e4fe0a1c3172efb39486d50d
Simon Davy <bloodearnest@gmail.com>
2013-07-03 17:36:43 +0100

    Added setup.py and registered

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "turgles",
+    version = "0.1",
+    author = "Simon Davy",
+    author_email = "bloodearnest@gmail.com",
+    description = ("An OpenGL ES renderer for LOGO style turtles."),
+    license = "MIT",
+    keywords = "turtle education kids opengl",
+    url = "https://github.com/AllTheWayDown/turgles",
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Education",
+        "Topic :: Games/Entertainment :: Simulation",
+    ],
+)
