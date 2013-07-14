be0fe350fa4d87654a26d90971378cc1220c4c79
Simon Davy <bloodearnest@gmail.com>
2013-07-14 23:34:08 +0100

    Purty tables for measuring

--- turgles/util.py
+++ turgles/util.py
@@ -55,45 +55,54 @@ class measure(object):
 @atexit.register
 def print_measurements():
     import pyglet
-    print('fps: {:.3f}'.format(pyglet.clock.get_fps()))
-
+    print('FPS: {:.3f}'.format(pyglet.clock.get_fps()))
     data = []
     for name, values in MEASUREMENTS.items():
         values.sort()
-        mean, median, graph = stats(values)
-        data.append((median, mean, graph, name))
+        mean, median, dmin, dmax, graph = stats(values)
+        data.append((median, mean, dmin, dmax, graph, name))
     data.sort()
-    for median, mean, graph, name in data:
-        print("{}: {:.3f} ms\n{}".format(name, median*1000, graph))
+    hd  = "{:10}| {:>10} | {:>5} {:^32} {:>5}|"
+    fmt = "{:10}: {:7.1f} ms [ {:5.1f} {} {:5.1f}]"
+
+    print(hd.format('name', 'median', 'min', 'histogram', 'max'))
+    print('-' * 64)
+    for median, mean, dmin, dmax, graph, name in data:
+        print(fmt.format(name, median * 1000, dmin * 1000, graph, dmax * 1000))
 
 
-def stats(data):
+def stats(data, bins=32, outliers=2):
     data.sort()
-    dmin = data[0]
-    dmax = data[-1]
-    mean = sum(data) / len(data)
-    median = data[len(data) // 2]
+    outlier_min = data[0]
+    outlier_max = data[-1]
+    clean_data = data[outliers:-outliers]
+    dmax = clean_data[-1]
+    dmin = clean_data[0]
+    dmax = clean_data[-1]
+
+    mean = sum(clean_data) / len(clean_data)
+    median = data[len(clean_data) // 3]
     diff = dmax - dmin
 
-    bins = 64
-
     hist = [0] * bins
-    for num in data:
+    for num in clean_data:
         bin = int((num - dmin) / diff * bins)
         bin = min(bin, bins-1)
         hist[bin] += 1
 
     maxcount = max(hist)
 
-    ticks = ['▁', '▂', '▃', '▄', '▅', '▆', '▇', '█']
+    ticks = ['_', '▂', '▃', '▄', '▅', '▆', '▇']  #, '█']
 
     graph = []
     for count in hist:
         if count == 0:
             graph.append(' ')
+        elif count == 1:
+            graph.append(ticks[0])
         else:
-            index = int(count / maxcount * 8)
-            index = min(index, 7)
+            index = int(count / maxcount * 7)
+            index = min(index, 6)
             graph.append(ticks[index])
 
-    return mean, median, ''.join(graph)
+    return mean, median, outlier_min, outlier_max, ''.join(graph)
